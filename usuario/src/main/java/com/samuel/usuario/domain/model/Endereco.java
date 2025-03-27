6761fe0052425cf527884977371bc1d95e59ec9a
Samuel <samuel.thomas1109@outlook.com>
2025-03-26 21:30:10 -0300

    Refactor Endereco class with Lombok annotations.

--- usuario/src/main/java/com/samuel/usuario/domain/model/Endereco.java
+++ usuario/src/main/java/com/samuel/usuario/domain/model/Endereco.java
@@ -1,25 +1,18 @@
 package com.samuel.usuario.domain.model;
 
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.Embedded;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Endereco {
-    @Embedded
     private String telefone;
-    @Embedded
     private String rua;
-    @Embedded
     private String numero;
-    @Embedded
     private String cidade;
-    @Embedded
     private String cep;
-
-    public void setRua(String rua) {
-        if (rua == null || rua.trim().length() < 3) {
-            throw new IllegalArgumentException("O nome deve ter pelo menos 3 caracteres.");
-        }
-        this.rua = rua;
-    }
 }
