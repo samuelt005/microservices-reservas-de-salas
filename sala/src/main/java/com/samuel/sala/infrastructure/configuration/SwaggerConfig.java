bd6c0e8b401511512c82633e3e79bd074d1042dc
Samuel <samuel.thomas1109@outlook.com>
2025-04-09 19:40:38 -0300

    Add SwaggerConfig to define OpenAPI server URLs

---
+++ sala/src/main/java/com/samuel/sala/infrastructure/configuration/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.samuel.sala.infrastructure.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("/service-1"));
+    }
+}
